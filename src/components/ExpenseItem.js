1f2d0f8c7e2b3fc04808d520aa4827a536904c71
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-09-13 11:19:40 +0530

    styling in jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,22 +1,22 @@
+import "./ExpenseItem.css";
+
+
+
 function ExpenseItem() {
+    const expenseDate= new Date(2021,2,28);
+    const expenseTitle = 'car insurance';
+    const expenseAmount = 243;
+    const expenseLocation = 'London';
+
   return (
-    <div>
-      <div>
-        <h2>Food</h2>
-      <div>Rs 10</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+      <h2>{expenseLocation}</h2>
+
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
-
-      <div>
-        <h2>Petrol</h2>
-      <div>Rs 100</div>
-      </div>
-
-      <div>
-        <h2>Movies</h2>
-      <div>Rs 200</div>
-      </div>
-
-
     </div>
   );
 }
