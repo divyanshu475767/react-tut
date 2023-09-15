6f7b539fd3450dd4e4326941d8029aeeafcf81bd
divyanshu4757 <guptadivyanshu400@gmail.com>
2023-09-15 09:37:40 +0530

    Adding event listeners

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,12 +4,23 @@ import React from "react";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 
+
+
 const ExpenseItem=(props)=> {
   const expenseDate = props.date;
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
   const expenseLocation = props.location;
+ 
+  const clickHandler = ()=>{
 
+    console.log('clickeed');
+  }
+  
+  const deleteHandler =(e)=>{
+    e.target.parentNode.remove();
+  }
+  
   return (
   
     <div className="expense-item">
@@ -19,6 +30,9 @@ const ExpenseItem=(props)=> {
         expenseAmount={expenseAmount}
         expenseLocation={expenseLocation}
       />
+      <button onClick={clickHandler}>Change title</button>
+      <button onClick={deleteHandler}>Delete</button>
+
     </div>
    
   );
